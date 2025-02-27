6b158f0eb5cbfeb3ac4e1296146097d59f592b9b
2378654 <dario@andama.de>
2025-02-27 12:40:45 +0100

    fixed rotation for real rakel

--- Assets/Scripts/InputManager/Tracker/TrackerRakelRotation.cs
+++ Assets/Scripts/InputManager/Tracker/TrackerRakelRotation.cs
@@ -5,7 +5,9 @@ public class TrackerRakelRotation : FloatValueSource
 {
     public override void Update()
     {
-        float rotation = GameObject.Find("RenderedRakel").transform.eulerAngles.x;
-        Value = (rotation);
+        //float rotation = GameObject.Find("RenderedRakel").transform.eulerAngles.x;
+        //Value = (rotation);
+        //Value = (GameObject.Find("RotationValue").transform.rotation.x * 140) - 180;
+        Value = (GameObject.Find("RotationValue").transform.eulerAngles.z);
     }
 }
